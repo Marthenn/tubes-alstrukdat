4a142e62dba40a7d224f48881df16d800e14339c
Marthenn <bintang.dwimarthen@gmail.com>
2022-10-26 03:35:04 +0700

    feat: created a cat splash screen

---
+++ src/app/driver/driverDisplay.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../headers/display.h"
+
+int main(){
+    splashScreen();
+    return 0;
+}
---
+++ src/app/headers/display.h
@@ -0,0 +1,10 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+/**
+ * @brief Display a cat meowing splash scren
+ * 
+ */
+void splashScreen();
+
+#endif

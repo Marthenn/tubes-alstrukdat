197237b760e8fc5f49cea22425bf11c1d45735b1
Marthenn <bintang.dwimarthen@gmail.com>
2022-10-28 03:57:57 +0700

    feat: created template for commands file

---
+++ src/app/headers/commands.h
@@ -0,0 +1,16 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+/**
+ * @brief Initialize the simulator and load the config files
+ * 
+ */
+void Start();
+
+/**
+ * @brief Exit the simulator
+ * 
+ */
+void Exit();
+
+#endif

c90cca32d67aadc7972ebc1d844acae5e3a420b3
Marthenn <bintang.dwimarthen@gmail.com>
2022-10-30 02:30:11 +0700

    feat(comands): add Buy function [half done, need other depedencies {Dynamic List}]

--- src/app/headers/commands.h
+++ src/app/headers/commands.h
@@ -5,6 +5,7 @@
 #include "parser.h"
 #include "../../adt/headers/map.h"
 #include "../../adt/headers/liststatik.h"
+#include "../../adt/headers/simulator.h"
 
 /**
  * @brief Load the config files to simulator
@@ -20,5 +21,35 @@ void Start(Map* map, ListStatik* foods,  ListStatik* recipes, Word* name);
  * 
  */
 void Exit();
+
+/**
+ * @brief Go into the buy submenu if possible
+ * 
+ */
+void Buy(Simulator* simulator, ListStatik foods, ListStatik recipes);
+
+/**
+ * @brief Go into the mix submenu if possible
+ * 
+ */
+void Mix(Simulator* simulator, ListStatik foods, ListStatik recipes);
+
+/**
+ * @brief Go into the chop submenu if possible
+ * 
+ */
+void Chop(Simulator* simulator, ListStatik foods, ListStatik recipes);
+
+/**
+ * @brief Go into the fry submenu if possible
+ * 
+ */
+void Fry(Simulator* simulator, ListStatik foods, ListStatik recipes);
+
+/**
+ * @brief Go into the boil submenu if possible
+ * 
+ */
+void Boil(Simulator* simulator, ListStatik foods, ListStatik recipes);
 
 #endif
--- src/app/implementasi/commands.c
+++ src/app/implementasi/commands.c
@@ -4,13 +4,22 @@
 /* APP */
 #include "../headers/commands.h"
 
-void Start(Map* map, ListStatik* foods,  ListStatik* recipes, Word* name){
+void Start(Map* map, ListStatik* foods,  ListStatik* recipes, Simulator* simulator){
     ReadAllConfig(map, foods, recipes);
-    printf("Masukkan nama Anda: ");STARTWORD();*name = currentWord;
-    printf("Selamat datang ");DisplayWord(*name);printf("!\n");
+    printf("Masukkan nama Anda: ");STARTWORD();(*simulator).NamaPengguna = currentWord;
+    printf("Selamat datang ");DisplayWord((*simulator).NamaPengguna);printf("!\n");
 }
 
 void Exit(){
     printf("Exiting the simulator...\n");
     exit(0);
+}
+
+void Buy(Simulator* simulator, ListStatik foods, ListStatik recipes, Map map){
+    if(!(IsAdjacent((*simulator).Lokasi,T(map)))){
+        DisplayWord((*simulator).NamaPengguna);
+        printf(" tidak berada di area telepon!\n");
+    } else {
+        BuyMenu(foods, recipes);
+    }
 }
